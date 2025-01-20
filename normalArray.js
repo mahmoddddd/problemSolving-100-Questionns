5ccfee066ff65c9913b7732f8f03544c7ad6e51e
Mhmod Elsherif <90068731+mahmoddddd@users.noreply.github.com>
2025-01-20 21:49:27 +0200

    codewareProblems

--- normalArray.js
+++ normalArray.js
@@ -1,4 +1,3 @@
-//
 ////////////////////////////////////////////
 // implement array with ztom udemy course with my practical
 class MyArray {
@@ -59,4 +58,61 @@ function reverseString(str) {
   }
   return reversed;
 }
-console.log(reverseString("hi my name is mahmod")); // Output: 'domham si eman ym ih'
+console.log(reverseString("hi my name is mahmod1")); // Output: 'domham si eman ym ih'
+//// another way
+
+function reverce2(str) {
+  if (!str || str.length < 1 || typeof str !== "string") {
+    return "oohhhh somthin is wrong";
+  }
+  const backword = [];
+  const totleItems = str.length - 1;
+  for (let i = totleItems; i >= 0; i--) {
+    backword.push(str[i]);
+  }
+  console.log(backword);
+  return backword.join("");
+}
+console.log(reverce2("my name is mahmoood2"));
+
+// another way
+// Another way using split, reverse, and join
+function reverse3(str) {
+  return str.split("").reverse().join(""); // Split string into characters, reverse them, and join back to a string
+}
+console.log(reverse3("my name is mahmooood3"));
+
+// Another way using arrow function syntax
+const reverse4 = (str) => [...str].reverse().join(""); // Spread operator to create an array, then reverse and join
+console.log(reverse4("my name is mahmooood4"));
+
+///////////////////////////////////////////////////////////////////////
+
+// merge 2 arrays
+// Function to merge two arrays
+function mergeArray(arr1, arr2) {
+  const mergedArr = arr1.concat(arr2); // Use concat to merge the two arrays
+  const sortedArr = mergedArr.sort((a, b) => a - b);
+
+  return sortedArr;
+}
+console.log(mergeArray([1, 2, 30], [2, 7, 8, 5])); // Output: [1, 2, 2, 5, 7, 8, 30]
+// another way
+function mergeeArrayrSorted(arr1, arr2) {
+  const mergeeArrayr2 = [];
+  const array1Itme = arr1[0];
+  const array2Itme = arr2[0];
+
+  //   check
+  if (arr1.length === 0) {
+    return arr2;
+  }
+  if (arr2.length === 0) {
+    return arr1;
+  }
+
+  while (array1Itme || array2Itme) {}
+
+  return mergeeArrayr2;
+}
+mergeeArrayrSorted([1, 2, 5, 30], [2, 5, 8, 4]);
